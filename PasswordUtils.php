ff5ebb578b7b77a0e1ecf8181131d371a4c6a685
Kirk <textagroup@gmail.com>
2024-06-12 10:08:40 +1200

    Added error property and method to create user table

--- PasswordUtils.php
+++ PasswordUtils.php
@@ -13,15 +13,18 @@ class PasswordUtils
 {
     private $db;
 
+    private $error;
+
     function __construct() {
         $this->db = $this->connect();
+        $this->createUserTable();
     }
 
     /**
      * Method to connect to database with values loaded from .env file
      * TODO confirm it is only called once
      */
-    private function connect() {
+    private function connect(): mysqli {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
         $dotenv->load();
 
@@ -34,7 +37,7 @@ class PasswordUtils
 
         foreach ($keys as $key => $value) {
             if (!isset($_ENV[$value])) {
-                echo "A error has occure";
+                $this->error = "A error has occured";
                 throw new Exception("$key needs to exist in a .env file");
             }
             $$key = $_ENV[$value];
@@ -46,6 +49,17 @@ class PasswordUtils
         $this->db->close();
     }
 
-    // check if table exists
     // create db table
+    private function createUserTable(): bool {
+        $sql = "CREATE TABLE IF NOT EXISTS user (" .
+            "id int," .
+            "password varchar(255)," .
+            "salt varchar(64)," .
+            "Name varchar(64))";
+        $exists = $this->db->query($sql);
+        if (!$exists) {
+            $this->error = "User table error";
+            throw new Exception("Error creating user table");
+        }
+    }
 }
