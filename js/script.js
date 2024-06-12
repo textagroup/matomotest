531abd92b660cea02ddad8b6650fdd9babf45565
Kirk <textagroup@gmail.com>
2024-06-12 16:29:56 +1200

    Improving front end validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let pwError = document.getElementById('password_error');
 let pwConfirmError = document.getElementById('matching_password_error');
+let submit = document.getElementById('submit');
 
 pwError.style.display = 'none';
 pwConfirmError.style.display = 'none';
@@ -8,8 +9,10 @@ let password = document.getElementById('passwordBox');
 password.addEventListener('input', function() {
     if (password.value.length < 5 || password.value.match(/\d+/) === null) {
         pwError.style.display = 'inline';
+        submit.disabled = true;
     } else {
         pwError.style.display = 'none';
+        submit.disabled = false;
     }
 });
 
@@ -17,7 +20,11 @@ let passwordConfirm = document.getElementById('confirmPassword');
 passwordConfirm.addEventListener('input', function() {
     if (password.value !== passwordConfirm.value) {
         pwConfirmError.style.display = 'inline';
+        submit.disabled = true;
     } else {
         pwConfirmError.style.display = 'none';
+        submit.disabled = false;
     }
 });
+
+// TODO Fix issues where the submit button is enabled but the values are false
