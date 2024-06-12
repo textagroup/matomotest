66c2c16d088ab276d5153218a01bc3ca010c8273
Kirk <textagroup@gmail.com>
2024-06-13 08:32:24 +1200

    Submit button improvements to JS validation

--- js/script.js
+++ js/script.js
@@ -6,25 +6,36 @@ pwError.style.display = 'none';
 pwConfirmError.style.display = 'none';
 
 let password = document.getElementById('passwordBox');
+
+// disable submit button if the form input fails validation
+function submitButtonActivated() {
+    if (!password.value || !passwordConfirm.value) {
+        submit.disabled = true;
+    } else if (password.value !== passwordConfirm.value) {
+        submit.disabled = true;
+    } else {
+        submit.disabled = false;
+    }
+}
+
+submitButtonActivated();
+
+// toggle the error messages on user input
 password.addEventListener('input', function() {
     if (password.value.length < 5 || password.value.match(/\d+/) === null) {
         pwError.style.display = 'inline';
-        submit.disabled = true;
     } else {
         pwError.style.display = 'none';
-        submit.disabled = false;
     }
+    submitButtonActivated();
 });
 
 let passwordConfirm = document.getElementById('confirmPassword');
 passwordConfirm.addEventListener('input', function() {
     if (password.value !== passwordConfirm.value) {
         pwConfirmError.style.display = 'inline';
-        submit.disabled = true;
     } else {
         pwConfirmError.style.display = 'none';
-        submit.disabled = false;
     }
+    submitButtonActivated();
 });
-
-// TODO Fix issues where the submit button is enabled but the values are false
