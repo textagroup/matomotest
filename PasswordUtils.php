7e5a3d13d6910047a5bd065afd489c43edbfc4a5
Kirk <textagroup@gmail.com>
2024-06-12 15:34:18 +1200

    Avoid PHPUnit env name clashes

--- PasswordUtils.php
+++ PasswordUtils.php
@@ -45,9 +45,9 @@ class PasswordUtils
         $dotenv->load();
 
         $keys = [
-            'host' => 'HOST',
-            'user' => 'USER',
-            'password' => 'PASSWORD',
+            'host' => 'DB_HOST',
+            'user' => 'DB_USER',
+            'password' => 'DB_PASSWORD',
             'db' => 'DB'
         ];
 
