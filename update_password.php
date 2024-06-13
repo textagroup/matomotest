b72c3492d232d6d9d109bcabba59a8cd1cabe363
Kirk <textagroup@gmail.com>
2024-06-14 10:32:02 +1200

    Insert a default row

--- update_password.php
+++ update_password.php
@@ -6,6 +6,8 @@ require_once('PasswordUtils.php');
 
 $passwordUtils = new PasswordUtils();
 
+$passwordUtils->insertUserRow(1, 'User', 'weakpassword');
+
 $passwordUtils->testCookie();
 
 $html = $passwordUtils->fetchTemplate();
